30637e49c2e31e15f395e8de259fed894cf6d72a
yupix <yupi0982@outlook.jp>
2024-11-08 06:02:59 +0000

    feat: SystemWebhookの型とモデルを追加

---
+++ mipac/models/system_webhook.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+
+from mipac.utils.format import str_to_datetime
+from mipac.types.system_webhook import ISystemWebhook, SystemWebhookEventTypes
+
+
+class SystemWebhook:
+    def __init__(self, raw_system_webhook: ISystemWebhook):
+        self.__raw_system_webhook: ISystemWebhook = raw_system_webhook
+
+    @property
+    def id(self) -> str:
+        return self.__raw_system_webhook["id"]
+
+    @property
+    def is_active(self) -> bool:
+        return self.__raw_system_webhook["is_active"]
+
+    @property
+    def updated_at(self) -> datetime:
+        return str_to_datetime(self.__raw_system_webhook["updated_at"])
+
+    @property
+    def latest_sent_at(self) -> datetime | None:
+        return str_to_datetime(self.__raw_system_webhook["latest_sent_at"]) if self.__raw_system_webhook["latest_sent_at"] else None
+
+    @property
+    def latest_status(self) -> str | None:
+         return self.__raw_system_webhook["latest_status"]
+
+    @property
+    def name(self) -> str:
+        return self.__raw_system_webhook["name"]
+
+    @property
+    def on(self) -> SystemWebhookEventTypes:
+        return self.__raw_system_webhook["on"]
+
+    @property
+    def url(self) -> str:
+        return self.__raw_system_webhook["url"]
+
+    @property
+    def secret(self) -> str:
+        return self.__raw_system_webhook["secret"]
---
+++ mipac/types/system_webhook.py
@@ -0,0 +1,22 @@
+from typing import TypedDict, Literal
+
+SystemWebhookEventTypes = Literal[
+    'abuseReport',
+    'abuseReportResolved',
+    'userCreated',
+    'inactiveModeratorsWarning',
+    'inactiveModeratorsInvitationOnlyChanged',
+]
+
+
+class ISystemWebhook(TypedDict):
+    id: str
+    is_active: bool
+    updated_at: str
+    latest_sent_at: str | None
+    latest_status: str | None
+    name: str
+    on: SystemWebhookEventTypes
+    url: str
+    secret: str
+
